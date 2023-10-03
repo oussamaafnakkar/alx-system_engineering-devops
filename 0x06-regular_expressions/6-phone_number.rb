8f097f6758442083544c48137c29ff060fe8a6ec
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2023-10-03 11:55:04 +0000

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10,10}$/).join
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$0} <phone_number>"
+  exit 1
+end
+
+# Input phone number from the command line argument
+phone_number = ARGV[0]
+
+# Regular expression pattern for a 10-digit phone number
+pattern = /^\d{10}$/
+
+# Perform the regular expression match
+if pattern.match?(phone_number)
+  puts phone_number
+else
+  puts ""
+end
