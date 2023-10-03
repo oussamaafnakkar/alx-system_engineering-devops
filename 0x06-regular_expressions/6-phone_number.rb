05c8793204070daadca9c3bbc5f961081a4c55f3
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2023-10-03 15:08:09 +0000

    Delete 0x06-regular_expressions directory

--- 0x06-regular_expressions/6-phone_number.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-# Use scan to find all matches of exactly 10 digits and join them
-matches = ARGV[0].scan(/^\d{10,10}$/)
-
-# Display the matches (joined together if multiple)
-puts matches.join
