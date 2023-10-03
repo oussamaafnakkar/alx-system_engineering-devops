bb5a1d09cdae347134c8ba73f3be6e89d6d7973f
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2023-10-03 11:58:50 +0000

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join
+puts ARGV[0].scan(/hbttn/).join
+puts ARGV[0].scan(/hbtttn/).join
+puts ARGV[0].scan(/hbttttn/).join
+puts ARGV[0].scan(/hbtttttn/).join
