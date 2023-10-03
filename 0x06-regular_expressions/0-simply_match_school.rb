a1b488fdbb202f153ea1ef11c776588c772ac147
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2023-10-03 11:50:35 +0000

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,20 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/School/).join
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Input string from the command line argument
+input_string = ARGV[0]
+
+# Regular expression pattern
+pattern = /School/
+
+# Perform the regular expression match
+if match = input_string.match(pattern)
+  puts match[0]
+else
+  puts ""
+end
