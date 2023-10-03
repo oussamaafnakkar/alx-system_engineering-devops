d0d09ef38ddde9718a97872dc92a0e58a6b33412
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2023-10-03 11:52:47 +0000

    Update 5-beginning_and_end.rb

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^h.n$/).join
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Input string from the command line argument
+input_string = ARGV[0]
+
+# Regular expression pattern
+pattern = /^h.n$/
+
+# Perform the regular expression match
+if pattern.match?(input_string)
+  puts input_string
+else
+  puts ""
+end
