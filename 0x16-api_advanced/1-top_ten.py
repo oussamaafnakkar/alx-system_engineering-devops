e052c99161107cd5b8cda8a73434ab2e3e5d551a
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2024-01-16 20:01:04 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+"""queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) Apple' +
+        'WebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    r = requests.get('https://www.reddit.com/r/{:}/hot.json?limit=10'.format(
+        subreddit), headers=headers, allow_redirects=False)
+    if r.status_code < 300:
+        json = r.json()
+        data_dict = json.get('data')
+        hot_list = data_dict.get('children')
+        for post in hot_list:
+            sub_data_dict = post.get('data')
+            print(sub_data_dict.get('title'))
+    else:
+        print(None)
