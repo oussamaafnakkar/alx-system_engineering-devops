92415972726cb10c310e3f786b02dfb273199adb
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2024-01-16 20:06:00 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscribers
-for a given subreddit"""
+"""Function that queries the Reddit API and returns the number of subscribers
+for a given subreddit."""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
+    """Function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'YourRedditUsername/1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
+    elif response.status_code == 404:
+        print(f"Subreddit '{subreddit}' not found.")
+        return 0
     else:
+        print(f"Error: {response.status_code}")
         return 0
