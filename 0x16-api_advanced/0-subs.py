06613b583f7e5a7b76de19ff6590fd00baa9eb71
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2024-01-16 20:19:06 +0000

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-0-subs.py - Fetches the number of subscribers
-for a given subreddit using the Reddit API.
+Contains the number_of_subscribers function
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
-    """
-    # Construct the URL for the subreddit information endpoint
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    # Set a custom User-Agent to avoid API restrictions
-    headers = {"User-Agent": "custom_user_agent"}
-
-    try:
-        # Send a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response to extract subscriber count
-            data = response.json()
-            subscribers_count = data['data']['subscribers']
-            return subscribers_count
-        else:
-            # Return 0 for invalid subreddits or other errors
-            return 0
-    except Exception as e:
-        # Print an error message if an exception occurs during the request
-        print(f"Error: {e}")
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
