a8dca9b56f3a75942dc5d993c364162e8f2a3f02
oussamaafnakkar <oussamaafnakkar2002@gmail.com>
2024-01-25 16:09:25 +0000

    alin

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,21 @@
-#Enable the user holberton to login and open files without errors
+# 1-user_limit.pp
+
+class user_limit {
+  exec { 'set_file_limit':
+    command => 'echo "holberton hard nofile 4096" >> /etc/security/limits.conf && echo "holberton soft nofile 4096" >> /etc/security/limits.conf',
+    path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    onlyif  => 'test $(grep -c "holberton.*nofile" /etc/security/limits.conf) -eq 0',
+  }
+
+  exec { 'set_pam_limits':
+    command => 'echo "session required pam_limits.so" >> /etc/pam.d/su',
+    path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    onlyif  => 'test $(grep -c "pam_limits.so" /etc/pam.d/su) -eq 0',
+  }
+}
+
+include user_limit
+Enable the user holberton to login and open files without errors
 
 # Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
